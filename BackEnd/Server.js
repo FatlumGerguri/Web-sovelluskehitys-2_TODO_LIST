0668e9d036ed3a803d6111e42a36163a72289fa4
Hussein <Hussein.Al-Bayati@metropolia.fi>
2023-02-27 19:40:38 +0200

    Hussein Backend JWT, session, cookie

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -25,6 +25,22 @@ app.use(cors({
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Session info
+const Session_secret = process.env.session_secret;
+console.log("123:" + Session_secret)
+
+app.use(
+    session({
+      key: 'userId',
+      secret: Session_secret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        expires: 60 * 60 * 24, // expires date for the cookie is 24h
+      },
+    }),
+);
+
 // Server info
 const Server_port = process.env.Server_port;
 const Server_host = process.env.Server_Host;
@@ -35,6 +51,9 @@ const user = process.env.Database_user;
 const password = process.env.Database_password;
 const DB_port = process.env.Database_DB_port;
 const database = process.env.Database_database;
+
+// JWT info
+const Jwt_key = process.env.JWT_Key;
 
 const db = mysql.createConnection({
   host: host,
@@ -50,8 +69,8 @@ db.connect(function(err) {
 });
 
 app.post('/register', (req, res) => {
-  const Email = req.body.Email;
-  const password = req.body.Password;
+  const username = req.body.username;
+  const password = req.body.password;
 
   //it worked by using bcrypt
 // let username = "joy00223";
@@ -72,6 +91,75 @@ app.post('/register', (req, res) => {
   });
 });
 
+const verifyJWT = (req, res, next) => {
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    res.send('You need a token');
+  } else {
+    jwt.verify(token, Jwt_key, (err, decoded) => {
+      if (err) {
+        res.json({auth: false, message: 'failed to authenticate'});
+      } else {
+        req.userId = decoded.id;
+        next();
+      }
+    });
+  }
+};
+
+app.get('/isUserAuth', verifyJWT, (req, res) => {
+  res.send('You are authenticated');
+});
+
+// checks the user is loggedIn
+app.get('/login', (req, res) => {
+  if (req.session.user) {
+    res.send({loggedIn: true, user: req.session.user});
+  } else {
+    res.send({loggedIn: false});
+  }
+});
+
+
+app.post('/login', (req, res) => {
+
+  const username = req.body.username;// it gets the data form the front end.
+  const password = req.body.password;
+
+  // it works
+//  let username = "joy00223";
+//  let password = "123";
+
+  db.query('SELECT * FROM User WHERE UserName = ?;',
+      username,
+      (err, result) => {
+        if (err) {
+          res.send({err: err});
+        }
+
+        if (result.length > 0) {
+          console.log('Password: ' + result[0].Password);// Remember this get the data from database and it need to be right like check the name (Password) result[0].Password
+          bcrypt.compare(password, result[0].Password, (error, response) => {
+            if (response) {
+              const id = result[0].Id; // gets the Id from the database
+              const token = jwt.sign({id}, Jwt_key, {
+                expiresIn: 300, // token expires in 5 minutes
+              });
+              req.session.user = result;
+
+              //res.send(result);
+              res.json({auth: true, token: token, result: result});
+            } else {
+              res.json({auth: false, message: 'wrong email/password'});
+            }
+          });
+        } else {
+          res.json({auth: false, message: 'No user exists'});
+        }
+      },
+  );
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!!');
 });
@@ -89,6 +177,7 @@ app.get('/data', (req, res) => {
 
 app.listen(Server_port,
     () =>
-        console.log('The server is listening at http://%s:%s', Server_host,Server_port));
+        console.log('The server is listening at http://%s:%s', Server_host,
+            Server_port));
 
 
